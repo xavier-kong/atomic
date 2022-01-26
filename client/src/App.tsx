3d897c62a3087eed7913b06d2aa722fcde3371a0
xavier-kong <kongxianchengxavier@gmail.com>
2022-01-26 09:23:47 +0800

    begin work on daily tracker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import DailyTracker from './components/DailyTracker';
 
+interface Habit {
+    name: string; // e.g. reading
+    description?: string; // e.g. Read a book
+    amount: number; // 30
+    unit: string; // minutes, times
+}
+
+const habits: Habit[] = [
+    {
+        name: 'read',
+        amount: 10,
+        unit: 'minutes',
+    },
+    {
+        name: 'leetcode',
+        amount: 1,
+        unit: 'times',
+    },
+];
+
 const App = () => (
     <div>
         <DailyTracker habits={habits} />
--- client/src/components/DailyTracker.tsx
+++ client/src/components/DailyTracker.tsx
@@ -1,19 +1,59 @@
 import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+// import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Checkbox from '@mui/material/Checkbox';
+// import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 interface Habits {
-    habits: [SingleHabit];
+    habits: SingleHabit[];
 }
 
 interface SingleHabit {
     name: string; // e.g. reading
-    description: string; // e.g. Read a book
-    type: string; // duration, monthly, weekly etc
+    description?: string; // e.g. Read a book
     amount: number; // 30
-    unit: string; // minutes
-    start: string; // 1/1/2022 10:30 timestamps?
-    // interval:
+    unit: string; // minutes, times
 }
 
-const DailyTracker = ({ habits }: Habits) => <p>{habits}</p>;
+const DailyTracker = ({ habits }: Habits) => (
+    <List>
+        {habits.map((habit) => (
+            <ListItem
+                secondaryAction={
+                    <Checkbox
+                        edge="end"
+                        // onChange={handleToggle(value)}
+                        // checked={checked.indexOf(value) !== -1}
+                        // inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                }
+            >
+                <ListItemText
+                    primary={habit.name}
+                    secondary={
+                        <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                        >
+                            {`for ${habit.amount} ${habit.unit}`}
+                        </Typography>
+                    }
+                />
+            </ListItem>
+        ))}
+    </List>
+);
 
 export default DailyTracker;
+
+/* 
+
+
+
+*/
