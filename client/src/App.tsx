d6eded8cb4db3507c30496f69d07dc1a4e2b78a4
xavier-kong <kongxianchengxavier@gmail.com>
2022-01-28 14:24:00 +0800

    working on change done mutation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import DailyTracker from './components/DailyTracker';
 import GET_HABITS from './graphql/queries/getHabits';
+import CHANGE_DONE from './graphql/mutations/changeDone';
 
 const App = () => {
     const [habits, setHabits] = React.useState([]);
+    const [changeDone] = useMutation(CHANGE_DONE, {
+        refetchQueries: [GET_HABITS, 'getHabits'],
+    });
 
     useQuery(GET_HABITS, {
-        onCompleted: (data) => {
-            setHabits(data.allHabits);
+        onCompleted: (res) => {
+            setHabits(res.allHabits);
         },
     });
+
+    const handleDone = async (data) => {
+        await changeDone(data);
+    };
 
     return (
         <div>
