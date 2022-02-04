63994b3d586a7365c5930ea9e571b777898d6ff9
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-04 09:04:11 +0800

    creating mock data

--- mock-server/db.js
+++ mock-server/db.js
@@ -1,76 +1,76 @@
 module.exports = {
     habits: [
-    {
-        id: 1,
-        name: 'Read',
-        amount: 10,
-        unit: 'minutes',
-    },
-    {
-        id: 2,
-        name: 'Leetcode',
-        amount: 1,
-        unit: 'times',
-    },
+        {
+            id: 1,
+            name: 'Read',
+            amount: 10,
+            unit: 'minutes',
+        },
+        {
+            id: 2,
+            name: 'Leetcode',
+            amount: 1,
+            unit: 'times',
+        },
     ],
     done: [
         {
-            id: 1,
+            habitId: 1,
             date: "29/1/2022",
             done: true
         },
         {
-            id: 1,
+            habitId: 1,
             date: "30/1/2022",
             done: true
         },
         {
-            id: 1,
+            habitId: 1,
             date: "31/1/2022",
             done: false
         },
         {
-            id: 1,
+            habitId: 1,
             date: "1/2/2022",
             done: true
         },
         {
-            id: 1,
+            habitId: 1,
             date: "2/2/2022",
             done: true
         },
         {
-            id: 1,
+            habitId: 1,
             date: "3/2/2022",
             done: true
         },
         {
-            id: 2,
+            habitId: 2,
             date: "29/1/2022",
             done: true
         },
         {
-            id: 2,
+            habitId: 2,
             date: "30/1/2022",
             done: true
         },
         {
-            id: 2,
+            habitId: 2,
             date: "31/1/2022",
             done: false
         },
         {
-            id: 2,
+            habitId: 2,
             date: "1/2/2022",
             done: false
         },
         {
-            id: 2,
+            habitId: 2,
             date: "2/2/2022",
             done: false
         },
         {
-            id: 2,
+            habitId: 2,
             date: "3/2/2022",
             done: true
         },
