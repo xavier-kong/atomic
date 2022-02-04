04f5c03ccb9a684f87b3621f3425b38d4b4b3f51
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-04 16:27:38 +0800

    updating schema to match current needs

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -1,26 +1,17 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE habits (
-  user_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY,
+  habit_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY,
 	habit_name VARCHAR(100) NOT NULL,
 	amount INT(255) NOT NULL,
   unit VARCHAR(100) NOT NULL,
-  UNIQUE(user_uid),
+  UNIQUE(habit_uid),
   UNIQUE(habit_name),
 );
 
 CREATE TABLE progress (
-	group_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY,
-	group_name VARCHAR(100) NOT NULL,
-	passphrase VARCHAR(200) NOT NULL,
-  expiry_date TIMESTAMP NOT NULL,
-  UNIQUE(group_uid),
-  UNIQUE(group_name),
-  UNIQUE(passphrase)
+  habit_uid UUID REFERENCES habits(habit_uid) NOT NULL
+	habit_date DATE NOT NULL,
+  done BOOLEAN NOT NULL
+  UNIQUE(habit_uid),
 );
-
-        {
-            habitId: 1,
-            date: "29/1/2022",
-            done: true
-        },
