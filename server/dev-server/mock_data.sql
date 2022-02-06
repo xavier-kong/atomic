833503dd72e4a04b508f99a291221d2f00be1bef
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-06 09:10:44 +0800

    beginning creation of mock data

--- server/dev-server/mock_data.sql
+++ server/dev-server/mock_data.sql
@@ -1,6 +1,16 @@
-insert into habits (habit_name, amount, unit) values ('Read', 10, 'minutes');
-insert into habits (habit_name, amount, unit) values ('Leetcode', 1, 'times');
+DO $$
+DECLARE temp_habit_uid UUID;
+BEGIN
+    INSERT INTO habits (habit_name, amount, unit) VALUES ('Read', 10, 'minutes')
+    RETURNING habit_uid INTO temp_habit_uid;
 
--- insert into progress (habit_uid, habit_date, done) values
+    INSERT INTO progress (habit_uid, habit_date, done) VALUES (temp_habit_uid, '2/2/2022', false);
 
+    INSERT INTO habits (habit_name, amount, unit) VALUES ('Leetcode', 1, 'times')
+    RETURNING habit_uid INTO temp_habit_uid;
+
+    INSERT INTO progress (habit_uid, habit_date, done) VALUES (temp_habit_uid, '2/2/2022', true);
+END $$;
+
+SELECT * FROM progress;
 SELECT * FROM habits;
