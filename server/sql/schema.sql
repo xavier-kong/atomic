6e755456f415190fdde9d18f629aef55a666bf7a
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-06 08:53:30 +0800

    fixed pg docker container not creating mock data

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -3,15 +3,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE habits (
   habit_uid UUID DEFAULT uuid_generate_v4 () NOT NULL PRIMARY KEY,
 	habit_name VARCHAR(100) NOT NULL,
-	amount INT(255) NOT NULL,
+	amount INT NOT NULL,
   unit VARCHAR(100) NOT NULL,
   UNIQUE(habit_uid),
-  UNIQUE(habit_name),
+  UNIQUE(habit_name)
 );
 
 CREATE TABLE progress (
   habit_uid UUID REFERENCES habits(habit_uid) NOT NULL,
 	habit_date DATE NOT NULL,
   done BOOLEAN NOT NULL
-  UNIQUE(habit_uid),
 );
