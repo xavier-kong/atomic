2a9d68a04e472e5691c3d305a4e55b4aa08730c5
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-09 08:36:27 +0800

    able to create mock data with sql

--- server/dev-server/mock_data.sql
+++ server/dev-server/mock_data.sql
@@ -1,10 +1,16 @@
 DO $$
 DECLARE temp_habit_uid UUID;
+DECLARE curr_date DATE;
+
 BEGIN
     INSERT INTO habits (habit_name, amount, unit) VALUES ('Read', 10, 'minutes')
     RETURNING habit_uid INTO temp_habit_uid;
 
-    INSERT INTO progress (habit_uid, habit_date, done) VALUES (temp_habit_uid, '2/2/2022', false);
+    curr_date := NOW();
+
+    INSERT INTO progress (habit_uid, habit_date, done)
+    SELECT temp_habit_uid, (curr_date - i), true
+    FROM GENERATE_SERIES(1, 7) as i;
 
     INSERT INTO habits (habit_name, amount, unit) VALUES ('Leetcode', 1, 'times')
     RETURNING habit_uid INTO temp_habit_uid;
@@ -14,15 +20,3 @@ END $$;
 
 SELECT * FROM progress;
 SELECT * FROM habits;
-
-
--- Need to figure out how to get date then manipulate via date - int from generate series 
-
--- INSERT INTO progress
-
--- SELECT id, concat('Product ', id) 
-
--- FROM GENERATE_SERIES(1, current_setting('my.number_of_products')::int) as id;
-
--- https://stackoverflow.com/questions/41305146/psql-generate-series-how-to-use-it-to-populate-multiple-columns-in-a-table
--- https://stackoverflow.com/questions/7794679/inserting-values-generate-series-how-can-i-reuse-cycle-the-numbers-e-g-1-2
