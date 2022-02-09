1b019f0c511bc0a844b1274ab0239de5007d882c
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-09 08:38:34 +0800

    mock data for both habits

--- server/dev-server/mock_data.sql
+++ server/dev-server/mock_data.sql
@@ -3,6 +3,7 @@ DECLARE temp_habit_uid UUID;
 DECLARE curr_date DATE;
 
 BEGIN
+
     INSERT INTO habits (habit_name, amount, unit) VALUES ('Read', 10, 'minutes')
     RETURNING habit_uid INTO temp_habit_uid;
 
@@ -15,7 +16,10 @@ BEGIN
     INSERT INTO habits (habit_name, amount, unit) VALUES ('Leetcode', 1, 'times')
     RETURNING habit_uid INTO temp_habit_uid;
 
-    INSERT INTO progress (habit_uid, habit_date, done) VALUES (temp_habit_uid, '2/2/2022', true);
+    INSERT INTO progress (habit_uid, habit_date, done)
+    SELECT temp_habit_uid, (curr_date - i), false
+    FROM GENERATE_SERIES(1, 3) as i;
+    
 END $$;
 
 SELECT * FROM progress;
