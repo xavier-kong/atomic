7ec5414b03c1f1b69c04dbbde6bc9466ce45d591
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-11 19:49:58 +0800

    forget to add prettierrc lol

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,15 @@
-console.log('hello!');
+import { PrismaClient } from '@prisma/client';
 
-export {}
+const prisma = new PrismaClient();
+
+async function main() {
+    // ... you will write your Prisma Client queries here
+}
+
+main()
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
