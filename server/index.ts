71bbdeb3bfc0ef14362fbb7cf6e77091a588a6b0
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-12 16:09:30 +0800

    installing server dependancies

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
 
 const prisma = new PrismaClient();
 
@@ -14,3 +18,21 @@ main()
     .finally(async () => {
         await prisma.$disconnect();
     });
+
+async function startApolloServer(typeDefs, resolvers) {
+    const app = express();
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+    await server.start();
+    server.applyMiddleware({ app });
+    await new Promise<void>((resolve) =>
+        httpServer.listen({ port: 4000 }, resolve)
+    );
+    console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+    );
+}
