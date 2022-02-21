a2eae2ace738b7f79cfcfc8fe0f2609f07676bd0
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-21 20:33:14 +0800

    its obvious that the answer is not obvious

--- server/graphql/typedefs/index.ts
+++ server/graphql/typedefs/index.ts
@@ -1,32 +1,45 @@
-import { gql } from 'apollo-server-core';
+// import { gql } from 'apollo-server-core';
 
-const typeDefs = gql`
-    type Habits {
-        habit_uid: ID!
-        habit_name: String!
-        amount: Int!
-        unit: String!
-        progress: [Progress]
-    }
+// const typeDefs = gql`
+//     type Habits {
+//         habit_uid: ID!
+//         habit_name: String!
+//         amount: Int!
+//         unit: String!
+//         progress: [Progress]
+//     }
 
-    type Progress {
-        progress_uid: String!
-        habit_uid: ID!
-        habit_date: String!
-        done: Boolean!
-    }
+//     type Progress {
+//         progress_uid: String!
+//         habit_uid: ID!
+//         habit_date: String!
+//         done: Boolean!
+//     }
 
-    fragment ProgressFragment on Progress {
-        progress_uid
-        habit_uid
-        habit_date
-        done
-    }
+//     fragment ProgressFragment on Progress {
+//         progress_uid
+//         habit_uid
+//         habit_date
+//         done
+//     }
 
-    type Query {
-        allHabits: [Habits]!
-        allProgress(habit_uid: ID!): [Progress]!
-    }
-`;
+//     type Query {
+//         allHabits: [Habits]!
+//         allProgress(habit_uid: ID!): [Progress]!
+//     }
+// `;
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadSchema } from '@graphql-tools/load';
 
+const getSchema = async () => {
+    const schema = await loadSchema('../schema.graphql', {
+        // load from a single schema file
+        loaders: [new GraphQLFileLoader()],
+    });
+    console.log(schema);
+
+    return schema;
+};
+
+const typeDefs = getSchema();
 export default typeDefs;
