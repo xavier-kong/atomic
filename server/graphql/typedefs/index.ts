e6c99a493f9536e00bdb099b86cf32bd03a69890
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-22 19:55:09 +0800

    it shouldnt be that difficult to import grapqhl files but i guess it just works lol

--- server/graphql/typedefs/index.ts
+++ server/graphql/typedefs/index.ts
@@ -1,49 +1,7 @@
-// import { gql } from 'apollo-server-core';
+import { loadFile } from 'graphql-import-files';
 
-// const typeDefs = gql`
-//     type Habits {
-//         habit_uid: ID!
-//         habit_name: String!
-//         amount: Int!
-//         unit: String!
-//         progress: [Progress]
-//     }
-
-//     type Progress {
-//         progress_uid: String!
-//         habit_uid: ID!
-//         habit_date: String!
-//         done: Boolean!
-//     }
-
-//     fragment ProgressFragment on Progress {
-//         progress_uid
-//         habit_uid
-//         habit_date
-//         done
-//     }
-
-//     type Query {
-//         allHabits: [Habits]!
-//         allProgress(habit_uid: ID!): [Progress]!
-//     }
-// `;
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-import { loadSchema } from '@graphql-tools/load';
-import { importSchema } from 'graphql-import';
-
-const getSchema = async () => {
-    const schema = await loadSchema('../schema.graphql', {
-        // load from a single schema file
-        loaders: [new GraphQLFileLoader()],
-    });
-
-    return schema;
-};
-
-// const typeDefs = async () => await getSchema();
-
-const typeDefs = importSchema('../schema.graphql');
+const typeDefs = loadFile('./graphql/schema.graphql');
 
 export default typeDefs;
-// export default getSchema;
+
+// https://www.npmjs.com/package/graphql-import-files
