8d1afd857e80c95fa265628702a8ba5273d8a8f7
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-22 20:15:47 +0800

    its funny how typescript is still causing me headaches

---
+++ server/graphql/resolvers/allHabits.ts
@@ -0,0 +1,44 @@
+import { context } from '../context';
+
+interface progress {
+    progress_uid: string;
+    habit_uid: string;
+    habit_date: string;
+    done: boolean;
+}
+
+export const allHabits = async () => {
+    const habits = await context.prisma.habits.findMany();
+    const newData = habits.map(async (habit) => {
+        const progress = await context.prisma.progress.findMany({
+            where: {
+                habit_uid: habit.habit_uid,
+            },
+            orderBy: {
+                habit_date: 'desc',
+            },
+        });
+        const updatedProgress = checkProgress(progress);
+        return {
+            ...habit,
+            progress: updatedProgress,
+        };
+    });
+    return newData;
+};
+
+const checkProgress = (progress: progress[]) => {
+    // check for missing dates
+    // add dates
+};
+
+/*
+get habits
+for each habit
+    check progress
+    if missing dates
+        add missing dates as false to db
+return habits
+
+    // check last date, if last date is not today, insert dates with done = false until today
+*/
--- server/graphql/resolvers/index.ts
+++ server/graphql/resolvers/index.ts
@@ -1,26 +1,12 @@
 import { context, Context } from '../context';
 import { Resolvers } from './resolvers-types';
+import { allHabits } from './allHabits';
 
 const resolvers: Resolvers = {
     Query: {
         allHabits: async () => {
-            // check last date, if last date is not today, insert dates with done = false until today
-            const data = await context.prisma.habits.findMany();
-            const newData = data.map(async (habit) => {
-                const progress = await context.prisma.progress.findMany({
-                    where: {
-                        habit_uid: habit.habit_uid,
-                    },
-                    orderBy: {
-                        habit_date: 'desc',
-                    },
-                });
-                return {
-                    ...habit,
-                    progress,
-                };
-            });
-            return newData;
+            const res = await allHabits();
+            return res;
         },
         allProgress: async (parent, args) => {
             const data = await context.prisma.progress.findMany({
